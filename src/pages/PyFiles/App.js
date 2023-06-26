6f4c93f6c22af36f87ee609d3c8ed03e91126881
rishimanoharan04 <119451716+rishimanoharan04@users.noreply.github.com>
2023-06-26 09:24:44 -0400

    Add files via upload

---
+++ src/pages/PyFiles/App.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import Navbar from "./Navbar"
+import Pricing from "./pages/Pricing"
+import Home from "./pages/Home"
+import About from "./pages/About"
+
+function App() {
+  let Component
+  switch(window.location.pathname){
+    case"/":
+      Component = Home
+      break
+    case "/pricing":
+      Component = Pricing
+      break
+    case "/about":
+      Component = About
+      break
+  }
+
+
+  return(
+    <>
+      <Navbar /> 
+      <Component />
+    </>
+  ) 
+}
+
+export default App
